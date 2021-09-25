39c8b23b2e63d3962fb8ee514b8ff594e2047e7b
sivashanmugam09 <thiru1610@gmail.com>
2021-09-25 16:43:32 +0530

    new lines

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class Sample {
+	
+		public static void main(String[] args) {
+			System.out.println("thiru");
+			System.out.println("thirumalai");
+			System.out.println("thirumalairajan");
+		}
+}
